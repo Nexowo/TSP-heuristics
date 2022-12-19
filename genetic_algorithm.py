a29cdee22254b515aa7c404e09a692cdee53602a
nexowo <jmaccou@outlook.fr>
2022-12-19 22:45:05 +0100

    hsbdfbskjf

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import point2D as p2d
 
 # Données du problème (générées aléatoirement)
-NOMBRE_DE_VILLES = 10
+NOMBRE_DE_VILLES = 5
 
 #paramètres de l'algorithme génétique
 nb_generations = 100 # nombre de générations
@@ -46,12 +46,12 @@ def croisement(indiv1 : list[p2d.point2D], indiv2 : list[p2d.point2D]):
 
 def mutation(indiv : list[p2d.point2D]):
 
-    i = random.randint(0, len(indiv))
-    j = random.randint(i, len(indiv))
+    i = random.randint(0, len(indiv)-1)
+    j = random.randint(0, len(indiv)-1)
 
-    middle = indiv[i:j]
-    middle.reverse()
-    indiv[i:j] = middle
+    temp = indiv[i]
+    indiv[i] = indiv[j]
+    indiv[j] = temp
 
     return indiv
 
@@ -62,34 +62,50 @@ def find_best_indiv(population:list[list[p2d.point2D]]):
             best_indiv = population[i]
     return best_indiv
 
+def validate(elem : list[p2d.point2D], list_not_sorted : list[p2d.point2D]):
+    nb_it = [0 for _ in range(len(elem))]
+    for element in elem:
+        nb_it[element.id]+=1
+    
+    for i in range(len(nb_it)):
+        if nb_it[i] == 0:
+            elem.append(list_not_sorted[i])
+        if nb_it[i] == 2:
+            elem.remove(list_not_sorted[i])
+
+    return elem
+
 solution = []
 instance = p2d.generate_instance(NOMBRE_DE_VILLES)
 
 for _ in range(nb_individus):
-    solution.append(instance)
+    solution.append(instance.copy())
     random.shuffle(solution[-1])
 
 best_indiv = find_best_indiv(solution)
 
 for i in range(nb_generations):
     #select = selection(solution, nb_individus)
-    print("Generation {i}")
+    print("Generation {}".format(i))
     for j in range(len(solution)):
         print(p2d.solution_id(solution[j]))
     new_s = []
-    for j in range(int(0.8*len(solution))):
+    random.shuffle(solution)
+    for j in range(0,int(0.8*len(solution)), 2):
         child_1, child_2 = croisement(solution[j],solution[j+1])
+        child_1 = validate(child_1, instance)
+        child_2 = validate(child_2, instance)
         new_s.append(child_1)
         new_s.append(child_2)
-        j+=1
 
     for j in range(int (0.2*len(solution))):
         new_s.append(mutation(solution[-j-1]))
 
     solution = new_s
+
     x = find_best_indiv(solution)
     if cal_fitness(x) < cal_fitness(best_indiv):
-        best_indiv = x
+        best_indiv = x.copy()
 
 p2d.print_solution(best_indiv)
 plt.show()
